246d209e56dcccc07fca39ea45bfa8f8be6c6813
zack <zack.j.hung@google.com>
2026-01-02 17:54:05 +0800

    add maven

---
+++ src/main/java/com/idv/pratice/App.java
@@ -0,0 +1,16 @@
+package com.idv.pratice;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    Collection a;
+    Set b;
+    Map c;
+    List d;
+  }
+}
